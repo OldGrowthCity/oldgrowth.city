9664f276a0066ece2242cf6b3cfc6620293ad2ac
Lydia Scarf <lydia@oldgrowth.city>
2024-06-18 14:29:52 -0400

    Add cute console log message

---
+++ static/script.js
@@ -0,0 +1,14 @@
+console.log(`
+                gay
+               queer
+            brand sites
+        mobile and web apps
+      open source and open data
+    discounts based on alignment
+ beautiful software that values you
+               queer
+               trans
+
+             <3 lydia
+
+`)
